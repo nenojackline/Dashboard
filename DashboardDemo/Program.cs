2b4f4c521f43d80848ae0a8512cb136fafbbac33
Jacque <Jacque@DESKTOP-VNPE1N4>
2023-03-07 12:25:01 +0300

    Add project files.

---
+++ DashboardDemo/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using DashboardDemo.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DashboardDemoContextConnection") ?? throw new InvalidOperationException("Connection string 'DashboardDemoContextConnection' not found.");
+
+builder.Services.AddDbContext<DashboardDemoContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<DashboardDemoContext>();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();;
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
